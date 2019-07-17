995d223a67795bb4b0353175026a2db66ab8bf0b
wesley-hall <hallwesley@outlook.com>
2019-07-17 11:02:22 +0100

    navbar JS separated for open and close

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,31 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     const navControl = document.querySelector('.nav-control i')
+    const navPrimary = document.querySelector('.nav-primary')
     const navSecondary = document.querySelector('.nav-secondary')
 
-    const toggleBurgerMenu = function () {
-        navControl.classList.toggle('fa-bars')
-        navControl.classList.toggle('fa-times')
-        navSecondary.classList.toggle('hidden-mobile')
+
+    let navBarOpen = false
+
+    const openBurgerMenu = () => {
+        navBarOpen = true
+        navControl.classList.remove('fa-bars')
+        navControl.classList.add('fa-times')
+        navPrimary.style.marginBottom = '0'
+        navSecondary.classList.remove('hidden-mobile')
+    }
+
+    const closeBurgerMenu = () => {
+        navBarOpen = false
+        navControl.classList.remove('fa-times')
+        navControl.classList.add('fa-bars')
+        navPrimary.style.marginBottom = '1em'
+        navSecondary.classList.add('hidden-mobile')
+    }
+
+    const toggleBurgerMenu = () => {
+        if (!navBarOpen) openBurgerMenu()
+        else closeBurgerMenu()
     }
 
     navControl.addEventListener('click', toggleBurgerMenu)
